b9e5a4e7e8045146ebe37861cbfb0af18542d525
Vladislav Kovalev <max2433186@gmail.com>
2020-05-03 10:57:26 +0300

    Add 'getData' and 'addFront'

--- script/script.js
+++ script/script.js
@@ -1,7 +1,62 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
 'use strict';
 
-const getData = () => {
+const getData = callback => {
 
-	
+	const request = new XMLHttpRequest();
+	request.open("GET", "./../dbHeroes-master/dbHeroes.json");
 
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) return;
+        
+        if (request.status === 200) {
+            callback(JSON.parse(request.response));
+        } else {
+            throw new Error(request.status + " " + request.statusText);
+        }
+    });
+
+    request.send();
 };
+
+
+const addCards = data => {
+    console.log(data);
+    const flipper = document.querySelectorAll('.flipper');
+    const front = document.querySelectorAll('.wrapper__image');
+    const nameHero = document.querySelectorAll('.name-hero > h1');
+
+    const addFront = (img, name, dataItem) => {
+        const newImg = document.createElement('img');
+        newImg.src = "dbHeroes-master/" + dataItem.photo;
+        img.append(newImg);
+        name.textContent = dataItem.name;
+    };
+
+    const addBack = () => {
+        
+    };
+
+    for (let i = 0; i < front.length; i++) {
+        addFront(front[i], nameHero[i], data[i]);
+    }
+
+    // front.forEach(item => {
+    //     addFront(item);
+    // });
+};
+
+
+
+
+
+
+
+
+
+
+
+getData(data => addCards(data));
+
