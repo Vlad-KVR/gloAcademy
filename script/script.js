9cd2f6500917c712cbaba9e95172c75524bcce68
Vladislav Kovalev <max2433186@gmail.com>
2020-05-07 00:54:29 +0300

    Edit 'pagination', now this is class and add comments for 'searchFilm'

--- script/script.js
+++ script/script.js
@@ -13,12 +13,11 @@
 
 
 const flipper = document.querySelectorAll('.flipper'),
-wrapperImage = document.querySelectorAll('.wrapper__image'),
-back = document.querySelectorAll('.back'),
-nameHero = document.querySelectorAll('.name-hero > h1');
-let currentPage = 1;
-let listFilms = [];
-
+	wrapperImage = document.querySelectorAll('.wrapper__image'),
+	back = document.querySelectorAll('.back'),
+	nameHero = document.querySelectorAll('.name-hero > h1');
+let listFilms = [],
+	listHero = [];
 
 //получение данных
 const getData = callback => {
@@ -39,13 +38,15 @@ const getData = callback => {
     setTimeout(() => request.send(), 1000);
 };
 
+
+
 //добавление информации в карточки
 const addCards = data => {
 
     console.log(data);
 
     const addFront = (img, name, dataItem) => {
-
+		if (!dataItem) return;
         const newImg = document.createElement('img');
         newImg.src = "dbHeroes-master/" + dataItem.photo;
         img.append(newImg);
@@ -55,7 +56,7 @@ const addCards = data => {
     };
 
     const addBack = (back, name, dataItem) => {
-
+		if (!dataItem) return;
         const div = document.createElement('div');
 
         for (let key in dataItem) {
@@ -92,6 +93,8 @@ const addCards = data => {
 
 };
 
+
+
 //удаление информации с карточек
 const removeCards = () => {
 
@@ -110,177 +113,209 @@ const removeCards = () => {
 
 
 //пагинация
-const pagination = (data, pageNumber) => {
-    const pagination = document.querySelector('.pagination');
+//используем только init() и refresh(newData), ну и set data
+class Pagination {
+	constructor(data) {
+		this._data = data;
+		this._pagination = document.querySelector('.pagination');
+		this._pageNumbers = document.querySelectorAll('.page-number');
+		this._countPage = Math.ceil(data.length / 10);
+		this._currentPage = 1;
+	}
 
-    let pageNumbers = document.querySelectorAll('.page-number');
-    for (let i = 0; i < 3; i++) {
-        if (!pageNumbers[i]) {
-            const copy = pageNumbers[0].cloneNode(true);
-            copy.classList.remove('page-active');
-            copy.textContent = i + 1;
-            pagination.append(copy);
+	get data() {
+		return this._data;
+	}
+
+	set data(newData) {
+		this._data = newData;
+		this._countPage = Math.ceil(newData.length / 10);
+	}
+	
+	pageNumbersRefresh() {
+		for (let i = 0; i < 3; i++) {
+			if (!this._pageNumbers[i]) {
+				const copy = this._pageNumbers[0].cloneNode(true);
+				copy.classList.remove('page-active');
+				copy.textContent = i + 1;
+				this._pagination.append(copy);
+			}
+		}
+		this._pageNumbers = document.querySelectorAll('.page-number');
+	}
+
+	togglePageActive() {
+		if ((typeof this._currentPage) === 'number') return;
+		console.log(this._pageNumbers);
+		console.log(this._currentPage);
+        this._currentPage.classList.toggle('page-active');
+	}
+	
+	startPage() {
+        this.togglePageActive();
+
+        this._pageNumbers.forEach((item, i) => item.textContent = i + 1);
+        this.removeStartPage();
+        this.removeLastPage();
+
+        this._currentPage = this._pageNumbers[0];
+        this.togglePageActive();
+
+        if (this._countPage > 3) {
+
+            this.addLastPage();
+    
+        } else if (this._countPage < 3) {
+			
+            if (this._pageNumbers[2]) this._pageNumbers[2].remove();
+            if (this._countPage < 2) this._pageNumbers[1].remove();
+    
         }
-    }
-    pageNumbers = document.querySelectorAll('.page-number');
+        this._pageNumbers = document.querySelectorAll('.page-number');
+	}
+	
+	lastPage(target) {
 
-    const countPage = Math.ceil(data.length / 10);
-
-    console.log(countPage);
-    
-
-    const togglePageActive = () => {
-        if ((typeof currentPage) === 'number') return;
-        currentPage.classList.toggle('page-active');
-    };
-
-    const startPage = target => {
-        togglePageActive();
-
-        pageNumbers.forEach((item, i) => item.textContent = i + 1);
-        removeStartPage();
-        removeLastPage();
-
-        currentPage = pageNumbers[0];
-        togglePageActive();
-
-        if (countPage > 3) {
-
-            addLastPage();
-    
-        } else if (countPage < 3) {
-    
-            pageNumbers[2].remove();
-            if (countPage < 2) pageNumbers[1].remove();
-    
-        }
-        pageNumbers = document.querySelectorAll('.page-number');
-
-    };
-
-    const lastPage = target => {
-
-        togglePageActive();
-        removeLastPage();
+        this.togglePageActive();
+        this.removeLastPage();
 
         const targetNumber = +target.textContent;
-        let j = pageNumbers.length > 2 ? -2 : -1;
-        pageNumbers.forEach(item => {
+        let j = this._pageNumbers.length > 2 ? -2 : -1;
+        this._pageNumbers.forEach(item => {
             item.textContent = targetNumber + j;
             j++;
         });
-        currentPage = pageNumbers[2];
-        togglePageActive();
+		this._currentPage = this._pageNumbers[2] ? this._pageNumbers[2] : this._pageNumbers[1];
+        this.togglePageActive();
 
-        if (countPage > 3) addStartPage();
-    };
-
-    const switchPage = target => {
+        if (this._countPage > 3) this.addStartPage();
+	}
+	
+	switchPage(target) {
 
         if (target.textContent === '1') {
-            startPage(target);
+            this.startPage(target);
             return;
-        } else if (target.textContent === countPage.toString()) {
-            lastPage(target);
+        } else if (target.textContent === this._countPage.toString()) {
+            this.lastPage(target);
             return;
         }
 
-        togglePageActive();
+        this.togglePageActive();
         
         const targetNumber = +target.textContent;
         let j = -1;
         for (let i = 0; i < 3; i++) {
-            pageNumbers[i].textContent = targetNumber + j;
+            this._pageNumbers[i].textContent = targetNumber + j;
             j++;
         }
-
-        currentPage = pageNumbers[1];
-        togglePageActive();
-        editPagination();
-    };
+		this._pageNumbers = document.querySelectorAll('.page-number');
+		
+		this._currentPage = this._pageNumbers[1];
+		this.togglePageActive();
+        this.editPagination();
+    }
     
-    const addLastPage = () => {
+    addLastPage() {
         if (document.getElementById('sp2')) return;
-        pagination.insertAdjacentHTML('beforeend', `<span id="sp2">...</span>
-                <a href="#" id="last-page" class="trigger">${countPage}</a>`);
-    };
+        this._pagination.insertAdjacentHTML('beforeend', `<span id="sp2">...</span>
+                <a href="#" id="last-page" class="trigger">${this._countPage}</a>`);
+    }
 
-    const removeLastPage = () => {
+    removeLastPage() {
         if (!document.getElementById('sp2')) return;
         document.getElementById('sp2').remove();
         document.getElementById('last-page').remove();
-    };
+    }
 
-    const addStartPage = () => {
+    addStartPage() {
         if (document.getElementById('sp1')) return;
-        pagination.insertAdjacentHTML('afterbegin', `<a href="#" id="start-page" class="trigger">1</a>
+        this._pagination.insertAdjacentHTML('afterbegin', `<a href="#" id="start-page" class="trigger">1</a>
                 <span id="sp1">...</span>`);
-    };
+    }
 
-    const removeStartPage = () => {
+    removeStartPage() {
         if (!document.getElementById('sp1')) return;
         document.getElementById('sp1').remove();
         document.getElementById('start-page').remove();
-    };
+    }
 
-    const editPagination = () => {
-        if (+pageNumbers[2].textContent === countPage) {
-            removeLastPage();
+    editPagination() {
+        if (+this._pageNumbers[2].textContent === this._countPage) {
+            this.removeLastPage();
         } else {
-            addLastPage();
+            this.addLastPage();
         }
 
-        if (+pageNumbers[0].textContent === 1) {
-            removeStartPage();
+        if (+this._pageNumbers[0].textContent === 1) {
+            this.removeStartPage();
         } else {
-            addStartPage();
+            this.addStartPage();
         }
-    };
+    }
 
-    const addNewCards = data => {
-        const topBound = +currentPage.textContent * 10,
-            bottomBound = +currentPage.textContent * 10 - 10;
-        console.log(data);
+    addNewCards(data) {
+        const topBound = +this._currentPage.textContent * 10,
+			bottomBound = +this._currentPage.textContent * 10 - 10;
+			
         const newData = data.filter((item, i) => i >= bottomBound && i < topBound);
         removeCards();
         addCards(newData);
-    };
-
-    const eventPagination = event => {
+	}
+	
+	eventPagination(event) {
         event.preventDefault();
         if (!event.target.classList.contains("page-active") &&
         (event.target.classList.contains('page-number') || event.target.classList.contains('trigger'))) {
-            switchPage(event.target);
-            addNewCards(data);
+            this.switchPage(event.target);
+            this.addNewCards(this._data);
         }
-    };
+	}
+	
+	startEvent() {
+		this._pagination.addEventListener('click', this.eventPagination.bind(this), false);
+	}
 
-    startPage();
-    
-    
-    pagination.removeEventListener('click', eventPagination);
-    pagination.addEventListener('click', eventPagination);
+	//remove по каким-то причинам не срабатывал и было два слушателя,
+	// пришлось отказаться от removeEvent() 
+	removeEvent() {
+		this._pagination.removeEventListener('click', this.eventPagination.bind(this), false);
+	}
 
-};
+	init() {		
+		this.pageNumbersRefresh();
+		this.startPage();
+		this.startEvent();
+	}
+
+	refresh(newData) {
+		this.data = newData;
+		this.pageNumbersRefresh();
+		this.startPage();
+	}
+}
+
+const pagination = new Pagination(listHero);
 
 
 //фильтер по фильмам
-const filterFilms = (data, searchFilm) => {
-    return data.filter(item => {
+const filterFilms = (data, searchFilm) => data.filter(item => {
         if (!item.movies) return false;
         return [...item.movies].some(film => film.toLowerCase() === searchFilm.toLowerCase());
     });
-};
 
 //поиск по фильмам
 const searchFilm = data => {
+	//данные
     const input = document.getElementById('search'),
         dropdown = document.querySelector('.dropdown'),
         formSearch = document.getElementById('form-search');
 
+	//заполняем list films списком фильмов
     data.forEach(item => listFilms = listFilms.concat(item.movies));
     listFilms = listFilms.filter((item, i) => listFilms.indexOf(item) === i && item !== undefined);
 
+	//показываем фильмы в выпадающем списке
     const showFilms = () => {
         dropdown.textContent = '';
 
@@ -298,19 +333,22 @@ const searchFilm = data => {
         });
     };
 
+	//вставляем выбранный фильм в инпут
     const handlerFilm = event => {
         const target = event.target;
         if (target.tagName.toLowerCase() === 'li') {
             input.value = target.textContent;
             dropdown.textContent = '';
-        }
+		}
+		formSearch.dispatchEvent(new Event("submit"));
     };
 
 
     input.addEventListener('input', showFilms);
     dropdown.addEventListener('click', event => {
         handlerFilm(event);
-    });
+	});
+	
     formSearch.addEventListener('submit', event => {
         event.preventDefault();
 
@@ -321,19 +359,17 @@ const searchFilm = data => {
 
         const newData = filterFilms(data, input.value);
         console.log(newData);
-        pagination(newData);
+        pagination.refresh(newData);
         removeCards();
         addCards(newData);
     });
 };
 
-
-
-
-
 getData(data => {
-    pagination(data);
-    addCards(data);
-    searchFilm(data);
+	listHero = data;
+	pagination.data = listHero;
+	pagination.init();
+	addCards(listHero);
+	searchFilm(listHero);
 });
 
